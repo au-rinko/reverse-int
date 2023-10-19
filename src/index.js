f2ea6e67f9e3e3ff598aa1db3240c3937f5984ce
au-rinko <indgi@tut.by>
2023-10-19 12:50:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
+    let res = '';
+    const str = n.toString();
+    if(n < 0){
+        for(let i=str.length; i > 1; i--){
+            if(str[0] == 0){
+                break;
+            }
+            res += str[i-1];
+          };
+          console.log(res);
+    }else{
+        for(let i=str.length; i > 0; i--){
+            if(str[0] == 0){
+                break;
+            }
+            res += str[i-1];
+          }; 
+    }
+    
+    
+    return res;
 }
